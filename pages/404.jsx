4bd732de8e01f531930a76f97b5d121064ee9d0a
Ramazan <airtribunal@gmail.com>
2022-11-01 15:24:41 +0300

    Updated 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,17 +1,28 @@
+import Head from "next/head";
+import Link from "next/link";
 
 const NotFound = () => {
-    return (
-        <div className="error-contaniner">
-            <div className="error-content">
-                <h1 className="error-title">404</h1>
-                <h3 className="error-notfound">Not Found</h3>
-                <p className="error-decription">The resource requested could not be found on the server</p>
-                <button className="error-button">
-                    Back
-                </button>
-            </div>
+  return (
+    <>
+      <Head>
+        <title>404 Error</title>
+      </Head>
+      <div className="error-contaniner">
+        <div className="error-content">
+          <h1 className="error-title">404</h1>
+          <h3 className="error-notfound">Not Found</h3>
+          <p className="error-decription">
+            The resource requested could not be found on the server
+          </p>
+          <button className="error-button">
+            <Link className="error-link" href="/">
+              Back
+            </Link>
+          </button>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
